6d55ef0a654f17fee3a16a199566a21fcae0473b
daloba-epitech <daloba.minthe@epitech.eu>
2026-03-19 16:53:59 +0100

    rendu final klivio maj

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      navy: "#152237",
+      berry: "#8E0839",
+      blush: "#FFAFCC",
+      baby: "#BDE0FE",
+      sky: "#A2D2FF",
+      white: "#FFFFFF",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ tailwind/vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/W-INT-100-LIL-1-1-integration-26/tailwind/',
+})
